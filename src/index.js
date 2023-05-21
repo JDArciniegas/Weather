5100c3414bdfdf1853577e932b15e1cbc95ad74b
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-05-20 23:06:37 -0600

    set up webpack, bootsrap

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "bootstrap";
+import "./style.css";
+
+function component() {
+  const element = document.getElementById("content");
+
+  // Lodash, now imported by this script
+
+  return element;
+}
+
+document.body.appendChild(component());
